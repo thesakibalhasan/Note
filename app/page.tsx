04d93ed012f5ab415b8b662a1a848af3455988ed
Sakib Al Hasan <iamthesakibalhasan@gmail.com>
2026-01-01 12:59:38 +0600

    update ui

--- app/page.tsx
+++ app/page.tsx
@@ -131,6 +131,10 @@ import {
   CloudRain,
   Snowflake,
   MoreVertical,
+  ChartBarStacked,
+  LayoutGrid,
+  Rows3,
+  ShieldCheck,
 } from "lucide-react";
 import { useTheme } from "next-themes";
 import { CategoryManager } from "@/lib/category-manager";
@@ -175,6 +179,16 @@ interface FirebaseNote {
   images: string[];
   password?: string;
   isPasswordProtected: boolean;
+}
+
+import type { SVGProps } from 'react';
+import { Icon } from "@iconify/react";
+
+export function FluentMdl2EditCreate(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={12} height={12} viewBox="0 0 2048 2048" {...props}><path fill="white" d="M226 701q-48-48-73-109t-25-128q0-71 26-131t71-105t107-70t131-26q66 0 128 23t110 71l353 353l-91 91l-195-195l-293 293l195 195l-91 91zm238-443q-43 0-81 15t-66 44t-44 65t-17 82q0 38 10 66t29 53t41 47t48 47l293-293q-26-25-47-48t-46-40t-52-28t-68-10m1584 1790l-633-158l-293-293l91-91l217 218q16-52 44-98t67-85t84-66t99-45l-218-217l91-91l293 293zm-176-176l-82-329q-47 10-87 32t-73 55t-55 73t-32 87zM1728 192q53 0 99 20t82 55t55 81t20 100q0 51-19 98t-56 83L763 1775q-9 59-37 108t-70 87t-95 57t-113 21H0v-128q11 0 23-3t22-9q25-13 41-33t25-44t13-50t4-53q0-59 20-112t58-96t86-70t109-37L1547 267q36-36 83-55t98-20M448 1920q40 0 75-15t61-41t41-61t15-75t-15-75t-41-61t-61-41t-75-15t-75 15t-61 41t-41 61t-15 75q0 104-64 192zm518-529q-32-63-75-106t-106-75l-221 221q62 24 109 71t72 110zm852-853q37-37 37-90q0-26-10-49t-27-40t-41-28t-49-10q-53 0-90 37l-759 758q61 36 103 78t78 103z"></path></svg>);
+}
+export function GgWebsite(props: SVGProps<SVGSVGElement>) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={12} height={12} viewBox="0 0 24 24" {...props}><g fill="white"><path fillRule="evenodd" d="M14 7a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm3 2h-2v6h2z" clipRule="evenodd"></path><path d="M6 7a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zm0 4a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2zm-1 5a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2H6a1 1 0 0 1-1-1"></path><path fillRule="evenodd" d="M4 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3zm16 2H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1" clipRule="evenodd"></path></g></svg>);
 }
 
 // Icon mapping function - moved outside component for better performance
@@ -320,6 +334,7 @@ const MemoizedNoteCard = React.memo(
 
     return (
       <>
+      
         <Tooltip>
           <TooltipTrigger asChild>
             <Card
@@ -1484,11 +1499,21 @@ export default function NotesApp() {
   const getSectionTitle = useCallback(() => {
     switch (activeSection) {
       case "home":
-        return "Shared Notes";
+        return <>Shared Notes</>;
       case "archive":
-        return "Archived Notes";
+        return (
+          <>
+            <Archive className="inline-block mr-2" /> Archived Notes
+          </>
+        );
       case "trash":
-        return "Trash";
+        return (
+          <>
+            <Trash className="inline-block mr-2" /> Trash
+          </>
+        );
+      default:
+        return null; // Always include a default case in React
     }
   }, [activeSection]);
 
@@ -1994,14 +2019,15 @@ export default function NotesApp() {
               </button>
             </div>
             <div className="text-xs text-muted-foreground text-center mt-6 pt-4 border-t">
-              Made by{" "}
+            <Icon icon="gg:website" className="inline mr-2"/>             
+             Made by{" "}
               <a
                 href="http://iamthesakibalhasan.netlify.app/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
+                className="text-blue-600 "
               >
-                <u>Sakib Al Hasan</u>
+                <u className="text-blue">Sakib Al Hasan</u>
               </a>
             </div>
           </form>
@@ -2022,6 +2048,7 @@ export default function NotesApp() {
               <h1 className="text-lg font-bold">Shared Notes</h1>
             </div>
             <p className="text-xs text-muted-foreground mt-1">
+              <Users className="h-4 w-4 mr-2 inline" />
               Everyone can see and edit
             </p>
           </div>
@@ -2073,6 +2100,7 @@ export default function NotesApp() {
 
             <div className="space-y-2">
               <h3 className="text-sm font-medium px-4 text-muted-foreground">
+                <ChartBarStacked className="h-4 w-4 mr-2 inline" />
                 Categories
               </h3>
               <ScrollArea className="h-[calc(100vh-450px)]">
@@ -2178,6 +2206,7 @@ export default function NotesApp() {
                 <h1 className="text-lg font-bold">Shared Notes</h1>
               </div>
               <p className="text-xs text-muted-foreground mt-1">
+                <Users className="h-4 w-4 mr-2 inline" />
                 Everyone can see and edit
               </p>
             </div>
@@ -2591,6 +2620,7 @@ export default function NotesApp() {
                       onClick={() => setViewMode("grid")}
                       className="flex-1 sm:flex-none"
                     >
+                      <LayoutGrid className="h-4 w-4 mr-2 inline" />
                       Grid
                     </Button>
 
@@ -2600,6 +2630,7 @@ export default function NotesApp() {
                       onClick={() => setViewMode("list")}
                       className="flex-1 sm:flex-none"
                     >
+                      <Rows3 className="h-4 w-4 mr-2 inline" />
                       List
                     </Button>
                   </div>
@@ -2752,7 +2783,9 @@ export default function NotesApp() {
                 ? "Remove Password"
                 : "Password Required"}
             </DialogTitle>
+
             <p className="text-muted-foreground mb-4">
+              <ShieldCheck className="h-6 w-6 inline" />
               {passwordDialog.action === "setPassword"
                 ? "Enter a password to protect this note"
                 : passwordDialog.action === "removePassword"
